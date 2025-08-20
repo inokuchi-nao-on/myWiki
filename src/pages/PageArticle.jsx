07a711f89b1e70d2bfdbdd3c2181fa924aaf4f88
井口 直 <inokuchi.nao.on@mynavi.jp>
2025-08-20 18:27:56 +0900

    first commit

---
+++ src/pages/PageArticle.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebar from '../components/sidebar';
+import ArticleDisplay from '../components/articleDisplay';
+
+export const PageArticle = () => {
+    return (
+        <>
+            <Sidebar/>
+            <ArticleDisplay/>
+        </>
+    )
+}
