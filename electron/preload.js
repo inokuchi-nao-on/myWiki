fe100d6d4feb71751ec6db4c7fc212d7912330c1
井口 直 <inokuchi.nao.on@mynavi.jp>
2025-08-21 14:31:38 +0900

    make backend

--- electron/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  readCategories: (dirPath) => ipcRenderer.invoke('read-categories', dirPath),
-  readArticles: (dirPath) => ipcRenderer.invoke('read-articles', dirPath),
-  readArticle: (filePath) => ipcRenderer.invoke('read-article', filePath),
-});
